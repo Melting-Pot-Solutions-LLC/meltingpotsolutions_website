7cce8b57eba897f9f6361117928b4e3585c73499
steveEECSrubin <konstantinrubin@engineer.com>
2017-03-28 21:11:07 -0400

    .

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n email: $email \n Message: $message";
-$recipient = "admin@themepure.net";
+$recipient = "konstantinrubin@engineer.com";
 $subject = "Add Your Subject Here";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
